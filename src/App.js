bcc5710e6beaa8e82aac71f4d84bcd3c16ac0f65
karthiksridhar22 <karthik.sridhar_ug24@ashoka.edu.in>
2023-10-04 18:55:12 +0530

    Mehfil Website
    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Home from './components/pages/Home'
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <Navbar />
+      <Routes>
+        <Route path = '/' exact element = {Home}/> 
+      </Routes>
+  
+    </>
   );
 }
 
